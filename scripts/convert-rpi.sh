910eecc877f4a113d8e3f8a69b50c4f88f444114
peakwinter <jacob@peakwinter.net>
2014-05-14 13:49:52 -0400

    finish ALARM RPi conversion script

--- scripts/convert-rpi.sh
+++ scripts/convert-rpi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ARCH=`lscpu | grep Architecture:`
-ACCARCH=("armv6h" "armv7h")
-if [[ ${ACCARCH[*]} =~ ${ARCH##* } ]]
+AARCH=`lscpu | grep Architecture:`
+ARCH=${AARCH##* }
+if [[ ${ACCARCH[*]} != "armv6l" ]]
     then
         :
     else
-        echo "You must be running an ARMv6 or ARMv7 system to use this script. If you believe you've received this message in error, please contact the developer."
+        echo "You must be running a Raspberry Pi (ARMv6) to use this script. If you believe you've received this message in error, please contact the developer."
         exit
 fi
 
@@ -22,11 +22,9 @@ echo "##     arkOS conversion script     ##"
 echo "## (c) 2014 The CitizenWeb Project ##"
 echo "#####################################"
 echo ""
-echo "This script will convert your Arch Linux system (running on an ARMv6 or ARMv7 device) to a full-featured arkOS distribution, with all accessory services."
+echo "This script will convert your Arch Linux ARM system (running on a Raspberry Pi) to a full-featured arkOS distribution, with all accessory services."
 echo ""
 echo "This process is IRREVERSIBLE and results cannot be guaranteed. Please don't interrupt the conversion process!"
-echo ""
-echo "If you use a platform requiring a uboot flash, you should be asked during the process to do this. Make sure you respond properly or flash it manually before you attempt to reboot."
 echo ""
 echo "Enter YES in all caps if you want to proceed, enter NO or anything else to exit the script."
 echo ""
@@ -42,10 +40,12 @@ done
 echo "STEP 1: Installing resource files"
 wget -nv -O /etc/pacman.d/mirrorlist https://raw.githubusercontent.com/cznweb/arkos/master/PKGBUILDs/core/pacman-mirrorlist/mirrorlist || { echo "STEP 1 didn't complete successfully -- Failed to download/install resource files. Check your Internet connection before trying again."; exit 1; }
 wget -nv -O /etc/pacman.conf https://raw.githubusercontent.com/cznweb/arkos/master/PKGBUILDs/core/pacman/pacman.conf || { echo "STEP 1 didn't complete successfully -- Failed to download/install resource files. Check your Internet connection before trying again."; exit 1; }
+sed -i /etc/pacman.conf -e "s|@CARCH[@]|armv6h|g"
 echo ""
 
 echo "STEP 2: Installing packages and running updates"
-pacman -Syu --force --noconfirm --noprogressbar nginx genesis beacon logrunner pacman pacman-mirrorlist python2-pillow python2-pip python2-psutil python2-iptables || { echo "STEP 2 didn't complete successfully -- pacman failed to install packages. Check your Internet connection before trying again."; exit 1; }
+pacman -R logrotate
+pacman -Syyu --force --arch armv6h --noconfirm --noprogressbar filesystem nginx genesis beacon logrunner pacman pacman-mirrorlist python2-pillow python2-pip python2-psutil python2-iptables || { echo "STEP 2 didn't complete successfully -- pacman failed to install packages. Check your Internet connection before trying again."; exit 1; }
 wget -nv -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/cznweb/arkos/master/scripts/nginx.conf || { echo "STEP 2 didn't complete successfully -- Failed to download/install resource files. Check your Internet connection before trying again."; exit 1; }
 mkdir /etc/nginx/sites-available &> /dev/null
 mkdir /etc/nginx/sites-enabled &> /dev/null
